f6900929959c5eea3f1a332b2847f4d723cc8741
UniqueIam <abhimanyukumar83097@gmail.com>
2025-10-25 22:46:45 +0530

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const bodyParser = require("body-parser")
 
 dotenv.config();
 require('./config/db');
@@ -8,6 +9,9 @@ require('./config/db');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/',async(req,res)=>{
     res.send('Hii From backend');
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+exports.verifyToken = async(req,res,next) =>{
+    try {
+        const token = req.header("Authorization")
+        if (!token) {
+            return res.status(401).json({ message: "No token, authorization denied" })
+        }
+        const decode = jwt.verify(token,process.env.JWT_SECRET);
+        if(!decode){
+            return res.status(409).json({message:'Invalid token'})
+        }
+
+        req.user = decode;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Token is not valid" })
+    }
+}
